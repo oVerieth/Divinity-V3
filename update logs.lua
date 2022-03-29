59431d2da4406b72c69b8df4161cc69c4927cf9d
oVerieth <98603971+oVerieth@users.noreply.github.com>
2022-03-29 21:58:15 +0600

    Add files via upload

---
+++ update logs.lua
@@ -0,0 +1,134 @@
+Update V3.42:
+1) Recoded buybot
+2) Recoded hitsounds
+3) Some gui fixes
+
+Update v3.41:
+1) Added 1 usefull check for keybinds and etc. stuff
+
+Update v3.4:
+*Updated default config
+
+1) Fully reworked keybind system
+2) a) Added new keybind, watermark, spectator list, hit station
+   b) Removed old keybinds
+   c) Added animation for divinity indicators
+3) Now sket indicators show real body yaw(m0nsterJ p120 men)
+4) Reworked hitmarker
+5) Fixed bullet tracers
+6) Improved anti-bruteforce on enemy shot
+7) Fixed bugs and errors
+8) Added roll aa on slowwalk 
+9) Improved desync jitter stability (don't use yaw modifier with it)
+10) Improved cvars cacheing
+
+Update v3.34:
+1) Several stability improvements
+
+Update v3.31:
+1)Added Team Check To Anti-Bruteforce(thanks to @m0nsterJ for bugreport)
+
+Update v3.30:
+1) Added Set Standing As General (disable other aa conditions to easier setting)
+
+Update v3.22:
+1) Improved Code(Removed Some Useless Features, Improved Codestyle)
+2) Now All Code Is Commented
+3) Removed Auto Anti-Resolver, Because Now Script Have Own Anti-Bruteforce(Update v3.10)
+4) Now Jump Scout in Rage Group
+5) Fixed Grammar Error
+
+Update Divinity v3.21:
+1) Added Jump Scout Fix
+2) Now Autobuy Doesn't Buy Flashbang
+
+Update Divinity v3.20:
+1) Fixed Rare Console Spams(+rep to @Sestain)
+2) Added Default Settings
+3) Fixed DT Fakelag Overriding
+
+Update Divinity v3.14:
+1) Added Local Tracers
+2) Fixed Clantag
+3) Fixed All Known Console Spams
+
+Update Divinity v3.14:
+1) Several stability improvements
+
+Update Divinity v3.13:
+1) Fully reworked jitter system
+2) Added yaw left and yaw right to better jitter effect
+3) Now you can properly control desync jitter with yaw left and right
+4) Improves in jitter desync stability after tickbase recharge (now you should control it with yaw left and right if you wanna higher range)
+
+Update Divinity v3.12:
+1) Reworked Manual and Desync side indicators
+
+Update Divinity v3.11:
+1) Added viewmodel in scope
+
+Update Divinity v3.10:
+1) Fixes in gui
+2) Improved Value Caching
+3) Improved Viewmodel and World Modulations
+4) Added Anti-Bruteforce (On Local Shot, On Enemy miss, Both)
+5) Added Checkbox to At Target
+6) Added New Animated Style of Divinity Indicators
+7) Added 2-side Animation to Custom Scope
+8) Added Animated Divinity Clantag
+9) Several Fixes in Script Stability
+
+Update Divinity v3.02:
+1) Fixed rare console spam
+
+Update Divinity v3.01:
+1) Fixed critical bug when menu broke after returning to main gui
+
+Update Divinity v3.00:
+General:
+1) New gui
+2) Fully reworked code
+3) Big Optimization Improvements
+4) Fixed Biggest Part Of Bugs
+-------------------------------------------------
+Other:
+1) added tank jitter
+2) improved fake flick
+3) added custom fake flick
+4) added outline to fonts
+5) reworked and improved conditional switching
+5.1) bug fixed into air condition
+5.2) improved moving switch
+6) added custom scope
+7) fixed stand velocity treshold (now main standing -> moving condition)
+8) now on land you have move anti-aims
+9) added standbly choke
+10) big improves into jitter work
+11) clear yaw without jitter improves
+12) buybot now work with presets
+13) fixed console spam
+14) fixed some checkbox errors
+15) new skeet indicators(fully reworked)
+16) reworked world modulation
+17) added autodirection
+18) added lc and fakelag and bot flags
+19) improved fake indicator color
+20) really big improves in custom fake flick and fake flick stability
+21) changed slowwalk condition switch 
+22) added roll aa(only for standing, we are wait for a Polak)
+23) new legit aa, manuals, freestand system priority
+24) added damage and heartmarker
+25) added auto random lby
+26) added lby disable on exploit
+27) added damge indicator
+28) added new keybinds
+29) added adaptive scope
+30) added flag with show if enemy can shot faster
+31) added animated third person
+32) fully reworked leg fucker
+33) added custom hitsounds(you should download them)
+34) added viewmodel editor
+35) added bomb info
+36) added anti-defensive
+37) added hitmarker
+------------------------------------------------------
